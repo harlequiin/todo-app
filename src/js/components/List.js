bd9292dd4c5494d78115d2ecd4fbcb87d1f23e1e
harlequiin <maris.udris.lv@gmail.com>
2017-01-27 01:18:22 +0200

    Added addItem() function

--- src/js/components/List.js
+++ src/js/components/List.js
@@ -8,14 +8,23 @@ export default class List extends React.Component {
     this.state = {
       listItems:[
         {id:1, text:'Something to do'}
-      ]
+      ],
+      total: 1
     }
-
+    this.addItem = this.addItem.bind(this);
+  }
+  addItem(e){
+    e.persist();
+    this.setState({
+      listItems: this.state.listItems.concat([{id: this.state.total+1, text: e.target.previousSibling.value}]),
+      total: this.state.total + 1
+    })
+    console.log(e.target.previousSibling.value);
   }
   render() {
     return (
       <div className="list">
-        <AddItem/>
+        <AddItem addItem={this.addItem}/>
         <ul className="list-content">
           <ListItem text="Something to do"/>
         </ul>
