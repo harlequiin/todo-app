37537bedc90bf3fe6a8d8b376d90c84f9758b54d
harlequiin <maris.udris.lv@gmail.com>
2017-01-26 02:46:42 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + "/src/js/index.js",
+  output: {
+    path: __dirname + "/src",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders:[
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions:['', '.js']
+  }
+}
