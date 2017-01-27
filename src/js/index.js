d7d05934dcbd7cf7fedb4e7794bbacf2d620b81b
harlequiin <maris.udris.lv@gmail.com>
2017-01-27 06:24:52 +0200

    Added basic routing functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link, hashHistory} from 'react-router';
 import App from './components/App';
 
 
+class AppRoute extends React.Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={App} />
+      </Router>
+    );
+  }
+}
+
 ReactDOM.render(
-  <App/>,
+  <AppRoute/>, 
   document.getElementById('app')
 )
