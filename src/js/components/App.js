5d8cbaec220552c1164764616a673dc83b34ee8d
harlequiin <maris.udris.lv@gmail.com>
2017-01-27 06:18:02 +0200

    App root component

---
+++ src/js/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import List from './List';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>ToDo list</h1>
+        <button className="save-list">Save list</button>
+        <List/>
+      </div>
+    )
+  }
+}
