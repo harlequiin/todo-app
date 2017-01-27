9f5331c7ec053fa8a3f213cb9398b5f428edb6b3
harlequiin <maris.udris.lv@gmail.com>
2017-01-27 06:18:30 +0200

    Refactored root component into it's own module

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './components/List';
+import App from './components/App';
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <h1>ToDo list</h1>
-        <button className="save-list">Save list</button>
-        <List/>
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(
   <App/>,
