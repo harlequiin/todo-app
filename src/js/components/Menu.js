5f9d34f580a58579f82f7634e7a96ed546dea36b
harlequiin <maris.udris.lv@gmail.com>
2017-01-28 08:59:35 +0200

    Added Router Links to menu items

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router';
 import ListItem from './ListItem';
 
 export default class Menu extends React.Component {
   render() {
     const data = this.props.route.data;
     const listItems = data.map((list) => 
-      <ListItem text={list.name}
+      <ListItem text={<Link to={"/"+list.id}>{list.name}</Link>}
                 key={list.id}
                 id={list.id}
                 total={list.total}
