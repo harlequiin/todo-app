4b8486880d2438421fd3bce830f9e0928bfa8925
harlequiin <maris.udris.lv@gmail.com>
2017-01-28 09:11:17 +0200

    Added a link to menu for every list

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
+import {Link} from 'react-router'
 
 
 export default class App extends React.Component {
@@ -8,6 +9,9 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <h1>ToDo list</h1>
+        <Link to="/">
+          <button className="save-list">Return to Lists</button>
+        </Link>
         {this.props.children}
       </div>
     )
